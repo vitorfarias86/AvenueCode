9db0fa845c976e6eed0e7f1c4d8cb2ce7abe6eac
Vitor Farias <vitorfarias86@gmail.com>
2017-11-20 16:49:05 -0200

    Retirando codigo de teste

--- src/main/java/com/avenuecode/Application.java
+++ src/main/java/com/avenuecode/Application.java
@@ -1,41 +1,13 @@
 package com.avenuecode;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.avenuecode.model.Image;
-import com.avenuecode.model.Product;
-import com.avenuecode.service.ImageService;
-import com.avenuecode.service.ProductService;
-
 @SpringBootApplication
-public class Application implements CommandLineRunner {
-
-	@Autowired
-	private ProductService productService;
-
-	@Autowired
-	private ImageService imageService;
+public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@Override
-	public void run(String... arg0) throws Exception {
-
-		Product pai = new Product("Chuck");
-
-		Product product1 = new Product("Sergey");
-
-		product1.setProduct(pai);
-		
-		Image image = new Image();
-		image.setProd(product1);
-		
-		this.productService.save(product1);
-		this.imageService.save(image);
-	}
 }
